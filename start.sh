5e66708aaadd5b99ce4e7a949f4190b3c2ca12a3
bryan@dea.gl <bryan@dea.gl>
2020-05-21 22:15:40 -0500

    network

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 docker rm -f fetch
 docker build -t deagle/fetch:stable .
-docker run --init --name="fetch" -d -p 5000:5000 deagle/fetch:stable
+docker run --init --name="fetch" --network="fetch-net" -d -p 5000:5000 deagle/fetch:stable

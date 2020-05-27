9826b7c35d8d6b6025ab0a41422a9e400a11976f
bryan@dea.gl <bryan@dea.gl>
2020-05-26 23:18:52 -0500

    minify

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,16 @@
+declare -a minify=("fetch/static/index.html" 
+                   "fetch/static/style.css"
+                   "fetch/app/templates/results.html"
+                   "fetch/app/templates/error.html"
+                   "fetch/app/templates/empty.html")
+
+for i in "${minify[@]}"
+do
+    echo "$i"
+    html-minifier --collapse-whitespace --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --use-short-doctype --minify-css true --minify-js true $i -o $i.min
+    mv $i.min $i
+done
+
 docker rm -f fetch
 docker build -t deagle/fetch:stable .
 docker run --init --name="fetch" --network="fetch-net"  --env NER=stanford-ner --restart always -d -p 5200:5000 deagle/fetch:stable

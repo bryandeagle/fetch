ea269b818352977e6c3840fe6f2ae718815b2eb2
Bryan Deagle <bryan.deagle@ni.com>
2020-07-18 13:45:28 -0500

    fixed things

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,4 @@ done
 
 docker rm -f fetch
 docker build -t deagle/fetch:stable .
-docker run --init --name="fetch" --network="fetch-net"  --ip 172.18.0.2 --env NER=stanford-ner --restart always -d -p 5200:5000 deagle/fetch:stable
+docker run --init --name="fetch" --network="fetch-net"  --ip 172.18.0.2 --env NER=stanford-ner --env IP=172.18.0.2 --restart always -d -p 5200:5000 deagle/fetch:stable
--- fetch/fetch.py
+++ fetch/fetch.py
@@ -1,4 +1,5 @@
 from app import app
+import os
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host=os.environ['IP'], debug=False)

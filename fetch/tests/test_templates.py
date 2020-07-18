65b23291007e9a1781a668e171240e9c2d2b6f90
Bryan Deagle <bryan.deagle@ni.com>
2020-07-18 13:47:12 -0500

    fixed things

--- fetch/tests/test_templates.py
+++ fetch/tests/test_templates.py
@@ -2,7 +2,7 @@ from jinja2 import Environment
 import os
 
 TEMPLATES_DIR = 'app/templates'
-env = Environment()
+env = Environment(autoescape=True)
 
 for file in os.listdir(TEMPLATES_DIR):
     full_file = os.path.join(TEMPLATES_DIR, file)

d2a2bd63fcb4688e4b7d4d76866b392e268be131
Bryan Deagle <bryan@dea.gl>
2021-03-30 22:03:57 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker rm -f fetch
-docker build -t deagle/fetch:stable .
-docker run --init --name="fetch" --network="fetch-net"  --ip 172.18.0.2 --env NER=stanford-ner --env IP=172.18.0.2 --restart always -d -p 5200:5000 deagle/fetch:stable
+docker build -q -t deagle/fetch:stable .
+docker run --init --name="fetch" --network="fetch-net"  --ip 172.18.0.2 --env NER=stanford-ner --env IP=172.18.0.2 --restart always -d -p 127.0.0.1:5200:5000 deagle/fetch:stable
